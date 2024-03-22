cdd7629b859acbc3092513495b478dad607cd241
Shazia91 <164497525+Shazia91@users.noreply.github.com>
2024-03-22 17:47:15 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,42 @@
+//camelCase
+let userName = "muntaha"
+console.log(userName);
+
+
+//snake_case
+let user_name ="Hamza"
+console.log(user_name);
+
+
+// PascaleCas
+let UserName = "spider man"
+console.log(UserName);
+
+//Arrays
+let students =["shazia","zara","hania"]
+let firrststudentname = students[1]
+let secondstudentname = students[0]
+let thirdstudentname = students[2]
+
+
+console.log(0)
+console.log(students)
+console.log(1)
+console.log(2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
